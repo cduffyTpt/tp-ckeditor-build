8ff0bbb289fb7370ada9ed55ed3ae8d57d105f7f
cduffyTpt <118805873+cduffyTpt@users.noreply.github.com>
2024-05-22 13:38:24 +0100

    forgot to build in production mode (#5)

--- build/ckeditor.d.ts
+++ build/ckeditor.d.ts
@@ -8,7 +8,7 @@ import { Bold, Code, Italic } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Image, ImageInsert, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
 import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
@@ -20,7 +20,7 @@ import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 import TpUploadAdapter from './adapters/tp-upload-adapter';
 declare class Editor extends ClassicEditor {
-    static builtinPlugins: (typeof TpUploadAdapter | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Essentials | typeof Image | typeof Code | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof Markdown | typeof Paragraph | typeof Heading | typeof PasteFromOffice | typeof Table | typeof TableToolbar | typeof CodeBlock | typeof TextTransformation)[];
+    static builtinPlugins: (typeof TpUploadAdapter | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Essentials | typeof Image | typeof Code | typeof ImageToolbar | typeof ImageInsert | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof Markdown | typeof Paragraph | typeof Heading | typeof PasteFromOffice | typeof Table | typeof TableToolbar | typeof CodeBlock | typeof TextTransformation)[];
     static defaultConfig: any;
 }
 export default Editor;
